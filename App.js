4209f0f6ca1c664ad91f9addf2cc8bde21806ded
sadeepaMedhan <sadeepamedhan823@gmail.com>
2022-09-24 10:31:03 +0530

    add native-base

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import HomePage from './screens/HomePage'
 import Login from './screens/Login'
+import {NativeBaseProvider} from "native-base";
 
 
 export default function App() {
   return (
-    <View>
+    <NativeBaseProvider>
       <Login/>
-    </View>
+    </NativeBaseProvider>
   )
 }
--- screens/Login.js
+++ screens/Login.js
@@ -1,54 +1,61 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { Heading, useToast, Button, VStack, Input, Stack } from "native-base";
 import React from 'react'
 
-export default function Login() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Login Page</Text>
-            <TextInput
-                style={styles.text_input}
-                placeholder='Username'
-            />
-            <TextInput
-                style={styles.text_input}
-                placeholder='Password'
-            />
 
-            <TouchableOpacity style={styles.btn} >
-                <Text style={styles.btn_text}>Login</Text>
-            </TouchableOpacity>
-        </View>
+
+export default function Login() {
+    const toast = useToast();
+
+    return (
+        <VStack h="100%" w="80%" m="10" space={4} alignItems="center" justifyContent="flex-start">
+            <Heading mb="10" size="lg">Welcome</Heading>
+            <Input mb="5" variant="rounded" placeholder="User Name" />
+            <Input mb="5" variant="rounded" placeholder="Password" />
+            <Button mt="10" w="100%" borderRadius="50" size="md" variant="solid"
+            onPress={() => toast.show({
+                title: "Login Unsuccessfull",
+                placement: "bottom"
+            })}>
+                LOGIN
+            </Button>
+
+        </VStack>
     )
 }
 
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems: 'center',
-        justifyContent: 'center',
+    container: {
+        flex: 1,
     },
     title: {
-        textAlign: 'center',
-        margin: 10
+        color: 'white'
     },
     text_input: {
-        width:'60%', 
+        width: '60%',
         borderWidth: 1,
         borderColor: 'gray',
         borderRadius: 10,
         padding: 10,
         margin: 40
     },
-    btn_text:{
-           
+    btn_text: {
+
     },
     btn: {
-       width:'60%', 
-       height:50,
-       justifyContent: 'center',
-       alignItems: 'center',
-       backgroundColor:'green',
-       borderRadius: 10,
-       margin: 50
+        width: '60%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        borderRadius: 10,
+        margin: 50
     }
 })
+
+
+
+
+
+
